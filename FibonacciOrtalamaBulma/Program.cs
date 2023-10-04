a4ffd142ed57f660a82cd0c4d5bdc2b25d303fe1
Meryem Talay <meyremtlly@gmail.com>
2023-10-04 17:04:03 +0300

    ucgen cizimi ve fibonacci ortalama bulma eklendi

---
+++ FibonacciOrtalamaBulma/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Kulanıcıdan alınan derinliğe göre fibonacci serisindeki rakamların ortalamasını alıp ekrana yazdıran uygulamayı yazınız.
+namespace ortalamaHesaplama // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ortalamasını hesaplamak istediğiniz fibonacci derinliğini giriniz ..");
+            
+            int x=Convert.ToInt32(Console.ReadLine());
+             if (x < 0)
+                {
+                    Console.WriteLine("Lütfen pozitif bir sayı giriniz.");
+                    return;
+                }
+            Fibonacci(x);
+        }
+
+        public static void Fibonacci(int depth){
+            int x;
+            float sum=0;
+            List<int> fibo=new List<int>();
+
+            for(int i=0;i<depth;i++){
+             
+                if(i>1){
+                    x=fibo[i-1]+fibo[i-2];
+                    fibo.Add(x);
+                }else{
+                    fibo.Add(i);
+                }
+            }
+
+            for(int i=0;i<fibo.Count;i++){
+            Console.WriteLine($"Fibonacci dizisinin {i+1}. elemanı: {fibo[i]}");
+               
+            }
+            sum=fibo.Sum();
+            Console.WriteLine("Fibonacci Ortalaması:"+sum/depth);
+        }
+
+    }
+}
---
+++ UcgenCizimi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//Kullanıcıdan alınan boyut bilgisine göre console'a Üçgen çizen uygulamayı yazınız.
+namespace ucgenCizme 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üçgeni çizmek istediğiniz boyutu veriniz");
+            int x= Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1;i<=x;i++){
+                for(int j=1;j<=i;j++){
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
