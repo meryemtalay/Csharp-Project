d3c5af7c17f2f6159b6b0b7824268a3a73207735
Meryem Talay <meyremtlly@gmail.com>
2023-10-04 21:47:07 +0300

    daire çizimi eklendi

---
+++ DaireCizimi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dairenin yarıçapını giriniz.");
+            float yaricap = float.Parse(Console.ReadLine());
+
+
+            for (int y = -(int)yaricap; y <= (int)yaricap; y++)
+            {
+                for (int x = -(int)yaricap; x <= (int)yaricap; x++)
+                {
+                    if (x * x + y * y <= yaricap * yaricap)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("-");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
