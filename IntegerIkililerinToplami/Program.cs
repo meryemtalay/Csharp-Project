18c275330b0894f9e5ad3f86bdaaaa8bca2223ae
Meryem Talay <meyremtlly@gmail.com>
2023-10-06 15:30:02 +0300

    integer ikililerin toplamı

---
+++ IntegerIkililerinToplami/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IntegerIkililerinToplami // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Integer sayıları aralarında boşluk bırakarak giriniz (örnek: 2 3 1 5 2 5 3 3):");
+        string input = Console.ReadLine();
+        // stringi boşluklara bölerek bir diziye atıyorum
+        string[] inputs = input.Split(' ');
+
+        for (int i = 0; i < inputs.Length; i += 2)
+        {
+            int num1 = int.Parse(inputs[i]);
+            int num2 = int.Parse(inputs[i + 1]);
+            int toplam = num1 + num2;
+
+            if (num1 != num2)
+            {
+                Console.Write(toplam + " ");
+            }
+            else
+            {
+                Console.Write(toplam * toplam + " ");
+            }
+        }
+        }
+    }
+}
